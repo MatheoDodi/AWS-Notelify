39528dff99dd55fbdb3a67d52c1b7d3326819959
MatthewDodi <matthew.dodi@gmail.com>
2019-08-25 21:12:10 -0700

    Added functionality to add notes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import { withAuthenticator } from "aws-amplify-react";
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      note: "Hello World"
+    }
+  ]);
+
   return (
-    <div>
-      App
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-l">AWS Notelify</h1>
+      <form className="mb3">
+        <input type="text" className="pa2 f4" placeholder="Write your note" />
+        <button className="pa2 f4" type="submit">
+          Add Note
+        </button>
+      </form>
+      <div>
+        {notes.map(note => (
+          <div key={note.id} className="flex items-center">
+            <li className="list pa1 f3">{note.note}</li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
