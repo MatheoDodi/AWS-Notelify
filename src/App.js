eac4bb9cfb4d00de1859cc62d2c95f3b50b21f9c
MatthewDodi <matthew.dodi@gmail.com>
2019-08-26 20:51:09 -0700

    Added querying

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { withAuthenticator } from "aws-amplify-react";
+import { API, graphqlOperation } from "aws-amplify";
+import { createNote } from "./graphql/mutations";
+import { listNotes } from "./graphql/queries";
 
 function App() {
-  const [notes, setNotes] = useState([
-    {
-      id: 1,
-      note: "Hello World"
-    }
-  ]);
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState("");
+
+  useEffect(() => {
+    const getNotes = async () => {
+      const { data } = await API.graphql(graphqlOperation(listNotes));
+      setNotes(data.listNotes.items);
+    };
+
+    getNotes();
+  }, []);
+
+  const handleChangeNote = e => setNote(e.target.value);
+
+  const handleAddNote = async e => {
+    e.preventDefault();
+    const input = {
+      note
+    };
+    const { data } = await API.graphql(graphqlOperation(createNote, { input }));
+    const newNote = data.createNote;
+    console.log(data);
+    setNotes([newNote, ...notes]);
+    setNote("");
+  };
 
   return (
     <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
       <h1 className="code f2-l">AWS Notelify</h1>
-      <form className="mb3">
-        <input type="text" className="pa2 f4" placeholder="Write your note" />
+      <form className="mb3" onSubmit={handleAddNote}>
+        <input
+          type="text"
+          className="pa2 f4"
+          placeholder="Write your note"
+          onChange={handleChangeNote}
+          value={note}
+        />
         <button className="pa2 f4" type="submit">
           Add Note
         </button>
